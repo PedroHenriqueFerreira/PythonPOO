feb7f81b53d038013072a9591ae4a1fe2f12b39c
PedroHenriqueFerreira <HPedro09062004@gmail.com>
2022-12-28 11:08:40 -0300

    Initial

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ class Person:
   def __init__(self, first_name, last_name, age):
     self.first_name = first_name
     self.last_name = last_name
+    # Protected
     self._age = age
+    # Private
+    self.__salary = 1000
   
   @classmethod
   def createPerson(cls, *args):
@@ -19,6 +22,18 @@ class Person:
   @age.setter
   def age(self, age):
     self._age = age
+    
+  @property
+  def salary(self):
+    return self.__salary
+  
+  @salary.setter
+  def salary(self, salary):
+    self.__salary = salary
+
+class User(Person):
+  def login(self):
+    print(f'User ${self.first_name} ${self.last_name} logged in')
   
 john = Person('John', 'Harris', 30)
 
@@ -26,3 +41,5 @@ mary = john.createPerson('Mary', 'Curie', 25)
 
 print(vars(john))
 print(vars(mary))
+
+help(Person)
